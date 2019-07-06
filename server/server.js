58cdb3356587afa0e4ad00d5ea95726f74d1056e
Dubina Dmitry <dubinich@pop-os.localdomain>
2019-07-06 17:50:58 +0300

    teamplate development

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-console.log("derver started on port 3000");
+var http=require("http");
+var fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+var app=express();
+
+app.get("/profile",function(req,res)
+{
+    console.log("Route /profile");
+    res.send("qqq");
+});
+
+app.listen(3000);
+console.log("server started on port 3000");
